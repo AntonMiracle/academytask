369e7d84f12a6a9e1e05b8fb02d3ced96764267b
AntonMiracle <b.anton.m.1986@gmail.com>
2020-03-01 18:11:39 +0100

    update MyExtension, change hex indication data

--- magicnumber/src/main/java/magicnumber/MyExtension.java
+++ magicnumber/src/main/java/magicnumber/MyExtension.java
@@ -1,14 +1,12 @@
 package magicnumber;
 
 public enum MyExtension {
-    JPG(0xFFD8FFE0,
-            "jpg")
-    , TXT(0x464F524D, "txt"), UNKNOWN(0, "");
+    JPG(new int[]{0xFF, 0xD8, 0xFF, 0xE0}, "jpg"), TXT(new int[]{0x46, 0x4F, 0x52, 0x4D}, "txt"), UNKNOWN(new int[1], "");
 
-    private final int start;
+    private final int[] start;
     private final String name;
 
-    public int getStart() {
+    public int[] getStart() {
         return this.start;
     }
 
@@ -21,7 +19,7 @@ public enum MyExtension {
         return this.name;
     }
 
-    MyExtension(int start, String name) {
+    MyExtension(int[] start, String name) {
         this.start = start;
         this.name = name.toLowerCase();
     }
