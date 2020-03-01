cfe67d570481d4f80861b08c2162b144149301f3
AntonMiracle <b.anton.m.1986@gmail.com>
2020-03-01 17:02:41 +0100

    Update App, add methods getExtensionFromFileName, getExtensionFromFile

---
+++ magicnumber/src/main/java/magicnumber/App.java
@@ -0,0 +1,23 @@
+package magicnumber;
+
+import java.nio.file.Path;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public Extension getExtensionFromFileName(String fileName){
+        return null;
+    }
+
+    public Extension getExtensionFromFile(Path path){
+        return null;
+    }
+}
